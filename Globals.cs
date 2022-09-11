1a652417f7877abd775e8d477bbf5d6d195e3345
Oni <74692051+CallieBrimStone@users.noreply.github.com>
2022-09-10 21:28:09 -0400

    Add files via upload

---
+++ Globals.cs
@@ -0,0 +1,108 @@
+﻿using Terraria;
+using Terraria.ModLoader;
+using Terraria.ID;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using System;
+using Terraria.GameContent.ItemDropRules;
+using System.Linq;
+using DarkValley.Items;
+using DarkValley.Items.Weapons.Melee;
+using DarkValley.Items.Weapons.Ranged;
+using DarkValley.Items.Weapons.Magic;
+using DarkValley.Items.Weapons.Summon;
+using DarkValley.Items.Weapons.Throwing;
+using DarkValley.Items.Weapons.Assassin.Caltrops;
+using DarkValley.Items.Weapons.Assassin.Knives;
+using DarkValley.Items.Weapons.Assassin.Scythes;
+using DarkValley.Items.Minerals.Golstra;
+using DarkValley.Items.Materials;
+
+namespace DarkValley
+{
+    public class GProjectile : GlobalProjectile
+    {
+        public override bool InstancePerEntity => true;
+
+        public static bool AnyProjectiles(int type)
+        {
+            for (int i = 0; i < 1000; i++)
+            {
+                if ((Main.projectile[i]).active && Main.projectile[i].type == type)
+                {
+                    return true;
+                }
+            }
+            return false;
+        }
+    }
+    public class GNPC : GlobalNPC
+    {
+        public override void ModifyNPCLoot(NPC npc, NPCLoot npcLoot)
+        {
+            if (npc.type == NPCID.EyeofCthulhu)
+            {
+                npcLoot.Add(ItemDropRule.Common(ModContent.ItemType<EyeOfBall>(), 5));
+            }
+
+            /*if (Array.IndexOf(new int[] { NPCID.WyvernBody, NPCID.WyvernBody2, NPCID.WyvernHead, NPCID.WyvernTail }, npc.type) > -1)
+            {
+
+                LeadingConditionRule downedGolem = new(new DownedGolemCondition);
+                downedGolem.OnSuccess(ItemDropRule.Common(ModContent.ItemType<AirSlayer>(), 50));
+                //leadingConditionRule.OnSuccess(/*Additional rules as new lines);
+                npcLoot.Add(downedGolem);
+            } commented because i don't know how to fix the bug known as getting items to drop after golem. -Stratis
+            */
+        }
+        public override void ModifyGlobalLoot(GlobalLoot globalLoot)
+        {
+            if (Main.hardMode == true && NPC.downedMechBossAny == true)
+                globalLoot.Add(ItemDropRule.Common(ModContent.ItemType<GolstraCrystal>(), 1, 16, 32));
+
+        }
+    }
+    public class GItem : GlobalItem
+    {
+        public override void SetDefaults(Item item)
+        {
+            if (item.type == ItemID.SpikyBall)
+            {
+                item.StatsModifiedBy.Add(Mod); // Notify the game that we've made a functional change to this item.
+                item.DamageType = ModContent.GetInstance<AssassinClass>();
+            }
+            if (item.type == ItemID.StarCannon)
+            {
+                item.StatsModifiedBy.Add(Mod);
+                item.damage = 60;
+                item.rare = ItemRarityID.Pink;
+            }
+            if (item.type == ItemID.SuperStarCannon)
+            {
+                item.StatsModifiedBy.Add(Mod);
+                item.damage = 75;
+                item.rare = ItemRarityID.Yellow;
+            }
+            // Change damage to 50!
+        }
+        public override void ModifyItemLoot(Item item, ItemLoot itemLoot)
+        {
+            
+            // In addition to this code, we also do similar code in Common/GlobalNPCs/ExampleNPCLoot.cs to edit the boss loot for non-expert drops. Remember to do both if your edits should affect non-expert drops as well.
+
+            // Boss Drop Edits
+            if (item.type == ItemID.EyeOfCthulhuBossBag)
+            {
+                foreach (var rule in itemLoot.Get())
+                {
+                    if (rule is OneFromOptionsNotScaledWithLuckDropRule oneFromOptionsDrop && oneFromOptionsDrop.dropIds.Contains(ItemID.DemoniteOre) && oneFromOptionsDrop.dropIds.Contains(ItemID.CrimtaneOre))
+                    {
+                        var original = oneFromOptionsDrop.dropIds.ToList();
+                        original.Add(ModContent.ItemType<EyeOfBall>());
+                        oneFromOptionsDrop.dropIds = original.ToArray();
+                    }
+                }
+            }
+        }
+    }
+}
---
+++ Titles/Titles.cs
@@ -0,0 +1,75 @@
+﻿using Terraria;
+using Terraria.ModLoader;
+using Terraria.ID;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using System;
+
+namespace DarkValley.Titles
+{
+    public class Titles : ModPlayer
+    {
+        public bool text;
+        public float alphaText = 255f;
+        public float alphaText2 = 255f;
+        public float alphaText3 = 255f;
+        public float alphaText4 = 255f;
+        public int BossID;
+
+        public override void ResetEffects()
+        {
+            text = false;
+        }
+
+        public override void PreUpdate()
+        {
+            if (!GProjectile.AnyProjectiles(ModContent.ProjectileType<Title>()))
+            {
+                alphaText = 255f;
+                alphaText2 = 255f;
+            }
+        }
+    }
+    public class Title : ModProjectile
+    {
+        public override string Texture => "DarkValley/Blank";
+
+        public override void SetDefaults()
+        {
+            Projectile.width = 1;
+            Projectile.height = 1;
+            Projectile.penetrate = -1;
+            Projectile.hostile = false;
+            Projectile.friendly = false;
+            Projectile.tileCollide = false;
+            Projectile.ignoreWater = true;
+            Projectile.timeLeft = 300;
+        }
+
+        public override void AI()
+        {
+            Titles modPlayer = Main.player[Projectile.owner].GetModPlayer<Titles>();
+            modPlayer.text = true;
+            modPlayer.BossID = (int)Projectile.ai[0];
+            Projectile.velocity.X = 0f;
+            Projectile.velocity.Y = 0f;
+            if (Projectile.timeLeft <= 45)
+            {
+                if (modPlayer.alphaText < 255f)
+                {
+                    modPlayer.alphaText += 10f;
+                    modPlayer.alphaText2 += 10f;
+                }
+                return;
+            }
+            if (Projectile.timeLeft <= 180)
+            {
+                modPlayer.alphaText -= 5f;
+            }
+            if (modPlayer.alphaText > 0f)
+            {
+                modPlayer.alphaText2 -= 5f;
+            }
+        }
+    }
+}
