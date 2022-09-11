0fdac28e4f1a431cb08fb15bf6ecffe6fff7d91c
Oni <74692051+CallieBrimStone@users.noreply.github.com>
2022-09-11 15:08:18 -0400

    Update TestRainbowTrail.cs

--- Primitives/TestRainbowTrail.cs
+++ Primitives/TestRainbowTrail.cs
@@ -5,7 +5,7 @@ using Terraria;
 using Terraria.ID;
 using Terraria.Graphics;
 
-namespace DarkValley.Primitives
+namespace OriginHeavenMod.Primitives
 {
     [StructLayout(LayoutKind.Sequential, Size = 1)]
     public struct TestRainbowTrail
